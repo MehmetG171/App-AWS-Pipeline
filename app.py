584d3963b26cb3f97c2e7776af0227f77a595a70
MehmetG171 <68816334+MehmetG171@users.noreply.github.com>
2025-01-02 13:26:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,11 @@ from wtforms.validators import DataRequired
 import os
 import subprocess
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', 'default_secret_key')
 
-
 APP_VERSION = "1.0"
 APP_DESCRIPTION = "Mehmet's application."
-
 
 def get_last_commit_sha():
     """Get the last commit SHA from Git or environment variable."""
@@ -29,12 +26,10 @@ def get_last_commit_sha():
     except Exception as e:
         return str(e)
 
-
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -45,7 +40,6 @@ def login():
 
     return render_template('login.html', form=form)
 
-
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
     last_commit_sha = get_last_commit_sha()
@@ -55,7 +49,6 @@ def healthcheck():
         "last_commit_sha": last_commit_sha
     }
     return jsonify(response), 200
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
